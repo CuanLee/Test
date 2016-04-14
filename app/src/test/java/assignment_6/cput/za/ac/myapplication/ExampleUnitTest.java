1b235cff934ed4598f213e4d97e9eeb825199610
CuanLee <cuanlee27@gmail.com>
2016-04-14 15:23:13 +0200

    Initial Commit

---
+++ app/src/test/java/assignment_6/cput/za/ac/myapplication/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package assignment_6.cput.za.ac.myapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
